b4bda304dca46d738a3daf47a8ba1cba740c1658
William Huang <107070711+imwilliamhuang@users.noreply.github.com>
2023-05-11 23:04:33 -0300

    Update crud.js

--- crud.js
+++ crud.js
@@ -1,67 +1,43 @@
-document.querySelector("#salvar").addEventListener("click", cadastrar);
+document.querySelector("#salvar").addEventListener("click", cadastrar)
 
-function cadastrar() {
-  const titulo = document.querySelector("#titulo").value;
-  const descricao = document.querySelector("#descricao").value;
-  const categoria = document.querySelector("#categoria").value;
-  const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
+function cadastrar(){
+    let marca = document.querySelector("#marca").value
+    let modelo = document.querySelector("#modelo").value
+    let preco = document.querySelector("#preco").value
+    let categoria = document.querySelector("#categoria").value
 
-  const tarefa = {
-    titulo,
-    descricao,
-    categoria,
-  }
-
-  if(!validar(tarefa.titulo, document.querySelector("#titulo"))) return
-  if(!validar(tarefa.descricao, document.querySelector("#descricao"))) return
-  
-  document.querySelector("#tarefas").innerHTML += createCard(tarefa)
-
-  modal.hide()
-
-}
-
-
-function validar(valor, campo){
-    if(valor == ""){
-        campo.classList.add("is-invalid")
-        campo.classList.remove("is-valid")
-        return false
+    const tarefa = {
+        marca,
+        modelo,
+        preco,
+        categoria,
     }
-    
-    campo.classList.remove("is-invalid")
-    campo.classList.add("is-valid")
-    return true
+
+    document.querySelector("#tarefas").innerHTML += gerarCard(tarefa)
+
 }
 
-function apagar(botao){
-    botao.parentNode.parentNode.parentNode.remove()
-}
-
-
-function createCard(tarefa) {
-  return `
-        <div class="col-lg-3 col-md-6 col-12">
-            <div class="card">
-                <div class="card-header">
-                    ${tarefa.titulo}
-                </div>
-                <div class="card-body">
-                    <p class="card-text">${tarefa.descricao}</p>
-                    <p>
-                        <span class="badge text-bg-warning">${tarefa.categoria}</span>
-                    </p>
-                    <a href="#" class="btn btn-success" title="Marcar como Concluída">
-                        <i class="bi bi-check-lg"></i>
-                        Finalizar
-                    </a>
-                    <a onClick="apagar(this)" href="#" class="btn btn-danger" title="Apagar Tarefa">
-                        <i class="bi bi-trash"></i>
-                        Apagar
-                    </a>
-                </div>
-            </div> <!-- Card -->
-        </div><!-- Col -->
-
-    ` //template literals
+function gerarCard(tarefa){
+    return `<div class="col-12 col-md-6 col-lg-3">
+                <div class="card">
+                    <div class="card-header">
+                        ${tarefa.marca}
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">${tarefa.modelo}</p>
+                        <p>
+                            <span class="badge text-bg-warning">
+                                ${tarefa.categoria}
+                            </span>
+                        </p>
+                        <p>${tarefa.preco} Reais</p>
+                        <a href="#" class="btn btn-success">
+                            <i class="bi bi-check-lg"></i>
+                        </a>
+                        <a href="#" class="btn btn-danger">
+                            <i class="bi bi-trash"></i>
+                        </a>
+                    </div>
+                </div> <!-- card -->
+            </div> <!-- col -->` 
 }
